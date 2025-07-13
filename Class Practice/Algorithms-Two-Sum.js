2c2ebd32388348e95d110dcb2010684524d6be01
HaimFC <haimtehem@gmail.com>
2025-07-13 10:18:31 +0300

    two sum (with 3 diff time complexity) and find dup (with 2 diff time-comlexity) algorithms added

---
+++ Class Practice/Algorithms-Two-Sum.js
@@ -0,0 +1,65 @@
+// Create function that receives an array of numbers and a target
+// The function checks if the sum of any two numbers equals the target
+
+//-----------------------------------------------------------------------------
+
+// Solution with time complexity of O(n^2), Memory Complexity is O(n + 1)
+const twoSum = function(arr, target){
+
+    for( let i in arr){ // O(n2)
+        for (let j in arr){
+            if( i != j && arr[i] + arr[j] === target){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+console.log(twoSum([2,7,11,15], 17));
+console.log(twoSum([2,7,11,15], 43));
+
+//-----------------------------------------------------------------------------
+
+// Solution with time complexity of O(nlogn + n), Memory Complexity is O(n + 2)
+const twoSumBetter = function(arr, target){
+    arr.sort((a,b) => a-b); //sort - nlogn
+    let rPointer = arr.length - 1;
+    let lPointer = 0;
+
+    while(rPointer > lPointer){ // O(n)
+        if(arr[rPointer] + arr[lPointer] < target){
+            lPointer++;
+        }
+        else if(arr[rPointer] + arr[lPointer] > target){
+            rPointer--;
+        }
+        else{
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(twoSumBetter([2,7,11,15], 17));
+console.log(twoSumBetter([2,7,11,15], 43));
+
+//-----------------------------------------------------------------------------
+
+// Solution with time complexity of O(n), Memory Complexity is O(2n)
+const twoSumEvenBetter = function(arr, target) {
+    const seen = new Set(); 
+
+    for (let num of arr) { //O(n)
+        let complement = target - num;
+        if (seen.has(complement)) {
+            return true;
+        }
+        seen.add(num);
+    }
+
+    return false;
+}
+
+console.log(twoSumEvenBetter([2,7,11,15], 17));
+console.log(twoSumEvenBetter([2,7,11,15], 43));
---
+++ Class Practice/Algorithms-find duplicates.js
@@ -0,0 +1,25 @@
+// Create a function that get an array and print its duplicates - O(n^2)
+
+const findDups = function(arr){
+    for (let i in arr){ // O(n^2)
+        for (let j in arr){
+            if(arr[i] === arr[j]  && i != j){
+                console.log(arr[i]);    
+            }
+        }
+    }
+}
+
+// Create a function that get an array and print its duplicates - O(nlogn+n)
+
+const findDupsBetter = function(arr) {
+    arr.sort((a, b) => a - b); // O(n log n)
+    for (let i = 0; i < arr.length - 1; i++) { // O(n)
+        if (arr[i] === arr[i + 1]) {
+            console.log(arr[i]);
+        }
+    }
+}
+
+//findDups([1,2,3,4,45,6,6,7,8,9,1,2,3]);
+findDupsBetter([1,2,3,4,45,6,6,7,8,9,1,2,3]);
