954572096c409e80dd0a71273029925678cda3ab
HaimFC <haimtehem@gmail.com>
2025-08-12 09:34:56 +0300

    Delete portfolio project in order to change for seperate repo

--- Portfolio-Project/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menuToggle = document.getElementById('menuToggle');
-  const nav = document.getElementById('mobileMenu');
-
-  menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('open');
-  });
-});
